d5f56d4160ec5de8e24a348e8e118e17ae205079
ajit <ajitrajasekharan@icloud.com>
2019-05-17 10:07:57 -0400

    model dir as param

--- bert_POS/bert_post_test.py
+++ bert_POS/bert_post_test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import numpy as np
 import torch
 import torch.nn as nn
@@ -13,31 +14,39 @@ from pytorch_pretrained_bert import BertModel
 from bert_post_common import *
 
 
-device = get_device() 
-tokenizer = get_tokenizer()
+def test_model(model_dir):
+	device = get_device() 
+	tokenizer = get_tokenizer()
 
-print("Loading model ...")
-model = torch.load("my_model/pytorch_model.bin")
-print("Loading model complete")
-print("Loading Pickling tags...")
-fp = open("my_model/tags.pkl","rb")
-tags_arr = pickle.load(fp)
-print("Loading Pickling tags complete")
-fp.close()
+	print("Loading model ...")
+	model = torch.load(model_dir + "/pytorch_model.bin")
+	print("Loading model complete")
+	print("Loading Pickling tags...")
+	fp = open("my_model/tags.pkl","rb")
+	tags_arr = pickle.load(fp)
+	print("Loading Pickling tags complete")
+	fp.close()
 
 
-while True:
-	print("Enter text:")
-	text = input()
-	rt_test_dataset = PosDataset(construct_input(text),tokenizer,tags_arr[0],tags_arr[1])
-	rt_test_iter = data.DataLoader(dataset=rt_test_dataset,
+	while True:
+		print("Enter text:")
+		text = input()
+		rt_test_dataset = PosDataset(construct_input(text),tokenizer,tags_arr[0],tags_arr[1])
+		rt_test_iter = data.DataLoader(dataset=rt_test_dataset,
                              batch_size=8,
                              shuffle=False,
                              num_workers=1,
                              collate_fn=pad)
 
 
-	ret_arr = test(model, rt_test_iter,tags_arr[0],tags_arr[1])
-	print(ret_arr)
+		ret_arr = test(model, rt_test_iter,tags_arr[0],tags_arr[1])
+		print(ret_arr)
 
-#print(open('result', 'r').read().splitlines()[:100])
+
+
+if __name__== "__main__":
+	if (len(sys.argv) < 2):
+		print("Specify model dir to load model")
+	else:
+		test_model(sys.argv[1])
+
--- bert_POS/bert_post_train.py
+++ bert_POS/bert_post_train.py
@@ -9,70 +9,83 @@ import nltk
 import pdb
 import pickle
 from pytorch_pretrained_bert import BertModel
+import sys
 
 from bert_post_common import *
 
 
 
+def train_model(model_dir):
 
-tagged_sents = nltk.corpus.treebank.tagged_sents()
-print(len(tagged_sents))
-print(tagged_sents[0])
-tags = list(set(word_pos[1] for sent in tagged_sents for word_pos in sent))
-tags = ["<pad>"] + tags
-tags_str = ','.join(tags)
-print(len(tags_str))
-print(tags_str)
-tag2idx = {tag:idx for idx, tag in enumerate(tags)}
-idx2tag = {idx:tag for idx, tag in enumerate(tags)}
-
-
-from sklearn.model_selection import train_test_split
-train_data, test_data = train_test_split(tagged_sents, test_size=.1)
-print(len(train_data), len(test_data))
-#pdb.set_trace()
+	tagged_sents = nltk.corpus.treebank.tagged_sents()
+	print(len(tagged_sents))
+	print(tagged_sents[0])
+	tags = list(set(word_pos[1] for sent in tagged_sents for word_pos in sent))
+	tags = ["<pad>"] + tags
+	tags_str = ','.join(tags)
+	print(len(tags_str))
+	print(tags_str)
+	tag2idx = {tag:idx for idx, tag in enumerate(tags)}
+	idx2tag = {idx:tag for idx, tag in enumerate(tags)}
 
 
-device = get_device() 
-tokenizer = get_tokenizer()
-print(device)
+	from sklearn.model_selection import train_test_split
+	train_data, test_data = train_test_split(tagged_sents, test_size=.1)
+	print(len(train_data), len(test_data))
+	#pdb.set_trace()
 
 
-model = Net(vocab_size=len(tag2idx),device = device)
-model.to(device)
-model = nn.DataParallel(model)
+	device = get_device() 
+	tokenizer = get_tokenizer()
+	print(device)
 
 
-train_dataset = PosDataset(train_data,tokenizer,tag2idx,idx2tag)
-eval_dataset = PosDataset(test_data,tokenizer,tag2idx,idx2tag)
+	model = Net(vocab_size=len(tag2idx),device = device)
+	model.to(device)
+	model = nn.DataParallel(model)
 
-train_iter = data.DataLoader(dataset=train_dataset,
+
+	train_dataset = PosDataset(train_data,tokenizer,tag2idx,idx2tag)
+	eval_dataset = PosDataset(test_data,tokenizer,tag2idx,idx2tag)
+
+	train_iter = data.DataLoader(dataset=train_dataset,
                              batch_size=8,
                              shuffle=True,
                              num_workers=1,
                              collate_fn=pad)
-test_iter = data.DataLoader(dataset=eval_dataset,
+	test_iter = data.DataLoader(dataset=eval_dataset,
                              batch_size=8,
                              shuffle=False,
                              num_workers=1,
                              collate_fn=pad)
 
 
-optimizer = optim.Adam(model.parameters(), lr = 0.0001)
+	optimizer = optim.Adam(model.parameters(), lr = 0.0001)
 
-criterion = nn.CrossEntropyLoss(ignore_index=0)
+	criterion = nn.CrossEntropyLoss(ignore_index=0)
 
-train(model, train_iter, optimizer, criterion)
-eval(model, test_iter,tag2idx,idx2tag)
+	train(model, train_iter, optimizer, criterion)
+	eval(model, test_iter,tag2idx,idx2tag)
 
 
-print("Saving model...")
-torch.save(model, "my_model/pytorch_model.bin")
-print("Model saved")
-tags_arr = [tag2idx,idx2tag]
-print("Pickling tags...")
-fp = open("my_model/tags.pkl","wb")
-pickle.dump(tags_arr,fp)
-fp.close()
-print("Pickling complete...")
-#print(open('result', 'r').read().splitlines()[:100])
+	print("Saving model...")
+	torch.save(model, model_dir + "/pytorch_model.bin")
+	print("Model saved")
+	tags_arr = [tag2idx,idx2tag]
+	print("Pickling tags...")
+	fp = open(model_dir +"/tags.pkl","wb")
+	pickle.dump(tags_arr,fp)
+	fp.close()
+	print("Pickling complete...")
+	#print(open('result', 'r').read().splitlines()[:100])
+
+
+if __name__== "__main__":
+	if (len(sys.argv) < 2):
+		print("Specify model dir to save")
+	else:
+		try:
+			os.mkdir(sys.argv[1])
+		except:
+			print("Directory already exists")
+		train_model(sys.argv[1])
